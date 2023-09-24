a76578625dae52d8211a359fa80016d11d789667
Shah Mostakim <shah.md.mostakim@gmail.com>
2023-09-24 08:51:26 -0400

    working on backend user registration
    Implementing JWT based authentication. added protected routes, user access token after successful registration. Using JWT implementation from "simplejwt" provider

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User 
+from rest_framework_simplejwt.tokens import RefreshToken  
 from .models import Product 
 
 
@@ -9,3 +10,43 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class UserSerializer(serializers.ModelSerializer): 
+    # serializing custom fields 
+    name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta: 
+        model = User 
+        # these custom fields are returned in access token response 
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin']  
+
+    # getter method for custom field '_id', basically returning the default 'id' field
+    # this makes user 'id' field consistent with other models in models.py file 
+    def get__id(self, obj):  
+        return obj.id
+    
+    # reusing user model's default is_staff field and defining it as 'isAdmin'
+    def get_isAdmin(self, obj):  
+        return obj.is_staff
+    
+    # getter method for custom field 'name'
+    # basically reusing user.firstname 
+    def get_name(self, obj): 
+        name = obj.first_name
+        if name == '': 
+            name = obj.email
+
+        return name 
+
+
+# extends current User Serializer and includes a token field 
+class UserSerializerWithToken(UserSerializer):  
+    token = serializers.SerializerMethodField(read_only=True)
+    class Meta: 
+        model = User 
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin', 'token'] 
+    
+    def get_token(self, obj): 
+        token = RefreshToken.for_user(obj) 
+        return str(token) 
--- api/views.py
+++ api/views.py
@@ -1,14 +1,39 @@
 
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view 
-from rest_framework.response import Response 
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser 
+from django.contrib.auth.models import User 
+from rest_framework.response import Response  
 
 from .products import products # static dummy data  
 from .models import Product # real data from DB
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken 
 
-# Create your views here.
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.hashers import make_password 
+
+
+# this class is responsible for customizing response token  
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer): 
+    # override the validate method 
+    def validate(self, attrs): 
+        data = super().validate(attrs) 
+        # loop through all fields of user model serializer and prepare response 
+        serializer = UserSerializerWithToken(self.user).data
+        for k,v in serializer.items():  
+            data[k] = v 
+
+        return data 
+
+# new view class for token customization, extending 'TokenObtainPairView' class 
+# that uses a new serializer class for token customization 
+class MyTokenObtainPairView(TokenObtainPairView): 
+    serializer_class = MyTokenObtainPairSerializer 
+
+
+# List of API routes 
 @api_view(['GET'])
 def getRoutes(request): 
     routes = [
@@ -24,13 +49,50 @@ def getRoutes(request):
     return Response(routes)   
 
 
+# user registration from forntend 
+@api_view(['POST'])
+def registerUser(request):
+
+    data = request.data #coming from frontend user registration form 
+
+    # in the frontend, we ask user to put name, email, and password only
+    user = User.objects.create(
+        first_name = data['name'],
+        username = data['email'], # we will not ask 'username' in user registration form, rather use email by default 
+        email = data['email'], 
+        password = make_password(data['password']) 
+    )
+
+    serializer = UserSerializerWithToken(user, many=False) 
+    return Response(serializer.data) 
+
+
+# profile of current user 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated]) # any logged in user has permission 
+def getUserProfile(request): 
+    user = request.user 
+    serializer = UserSerializer(user, many=False) 
+    return Response(serializer.data) 
+
+
+# list of all users 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated, IsAdminUser]) # logged in user has to be admin 
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data) 
+
+
+# list of products 
 @api_view(['GET'])
 def getProducts(request): 
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)  
 
-
+# detail information for single product 
 @api_view(['GET'])
 def getProduct(request, pk): 
     product = Product.objects.get(_id=pk)
