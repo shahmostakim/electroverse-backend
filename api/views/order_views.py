0488921d5c4276dea735a1bbd45a3911a975a595
Shah Mostakim <shah.md.mostakim@gmail.com>
2023-10-01 17:29:51 -0400

    backend bug fix in order creation

--- api/views/order_views.py
+++ api/views/order_views.py
@@ -12,7 +12,7 @@ from rest_framework import status
 
 
 @api_view(['POST'])
-@permission_classes(['IsAuthenticated'])
+@permission_classes([IsAuthenticated]) 
 def addOrderItems(request): 
     user = request.user 
     data = request.data 
@@ -53,6 +53,5 @@ def addOrderItems(request):
             product.countInStock -= item.qty 
             product.save() 
 
-    serializer = OrderSerializer(order, many=True) 
-
-    return Response(serializer.data)
+        serializer = OrderSerializer(order, many=False)  
+        return Response(serializer.data) 
