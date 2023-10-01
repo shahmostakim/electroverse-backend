87c31e90017d9eb7672c4dc670edb7d6979ee5e4
Shah Mostakim <shah.md.mostakim@gmail.com>
2023-10-01 16:39:07 -0400

    createOrder - backend endpoint with serializer
    Backend function for creating an order with orderItems received from frontend. Added serializer for order data. Next to implement: frontend action for firing up order creation upon clicking on 'place order' button

---
+++ api/views/order_views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser 
+
+from rest_framework.response import Response  
+
+from api.models import Product, Order, OrderItem, ShippingAddress 
+from api.serializers import ProductSerializer, OrderSerializer
+
+from rest_framework import status 
+
+
+@api_view(['POST'])
+@permission_classes(['IsAuthenticated'])
+def addOrderItems(request): 
+    user = request.user 
+    data = request.data 
+
+    orderItems = data['orderItems']
+    if orderItems and len(orderItems) == 0: 
+        return Response({'detail': 'No Order Items'}, status=status.HTTP_400_BAD_REQUEST)
+    else: 
+        # (1) create order
+        order = Order.objects.create(
+            user = user ,
+            paymentMethod = data['paymentMethod'],
+            taxPrice = data['taxPrice'],
+            shippingPrice = data['shippingPrice'],
+            totalPrice = data['totalPrice'],
+            # other fields are created by default, will be changed later 
+        ) 
+        # (2) create shipping address 
+        shipping = ShippingAddress.objects.create(
+            order = order,
+            address = data['shippingAddress']['address'],
+            city = data['shippingAddress']['city'],
+            postalCode = data['shippingAddress']['postalCode'],
+            country = data['shippingAddress']['country'], 
+        )
+        # (3) create order items, set order to orderItems relationship  
+        for i in orderItems: 
+            product = Product.objects.get(_id=i['productId'])
+            item = OrderItem.objects.create(
+                product=product,
+                order=order,
+                name=product.name,
+                qty=i['qty'],
+                price=i['price'],
+                image=product.image.url,
+            )
+            # (4) update stock 
+            product.countInStock -= item.qty 
+            product.save() 
+
+    serializer = OrderSerializer(order, many=True) 
+
+    return Response(serializer.data)
