71e260cb331f0e8bb18b46c86ddca9d61b17b8a7
Shah Mostakim <shah.md.mostakim@gmail.com>
2023-10-23 01:13:45 -0400

    updated settings for docker collectstatic

--- backend/settings.py
+++ backend/settings.py
@@ -173,11 +173,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # ========= settings for uploaded media files ===================
 STATICFILES_DIRS = [
     #BASE_DIR / 'static',
-    os.path.join(BASE_DIR, 'static'), 
+    #os.path.join(BASE_DIR, 'static'), 
     #os.path.join(BASE_DIR, '..', 'frontend/build/static'), 
 ]
 
