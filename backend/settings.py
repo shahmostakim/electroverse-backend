ca21236bc0ab3e1a6fec561fb50081116261bf27
Shah Mostakim <shah.md.mostakim@gmail.com>
2023-10-24 19:49:14 -0400

    added HTTPS settings for later use

--- backend/settings.py
+++ backend/settings.py
@@ -253,3 +253,15 @@ except:
         }
     } 
 
+# Deployment settings 
+'''
+# HTTPS settings 
+SESSION_COOKIE_SECURE = True 
+CSRF_COOKIE_SECURE = True 
+SECURE_SSL_REDIRECT = True 
+
+# HSTS settings 
+SECURE_HSTS_SECONDS = 31536000 # 1 year 
+SECURE_HSTS_PRELOAD = True 
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True 
+'''
